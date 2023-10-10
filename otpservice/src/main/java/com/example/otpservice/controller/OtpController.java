1244994041d78f70ce66f4438f9e1bfa5757b9de
Jahnavi Badam <jahnavibadam@gmail.com>
2023-10-11 00:29:34 +0530

    imports update

--- otpservice/src/main/java/com/example/otpservice/controller/OtpController.java
+++ otpservice/src/main/java/com/example/otpservice/controller/OtpController.java
@@ -2,6 +2,7 @@ package com.example.otpservice.controller;
 
 import com.example.otpservice.service.OtpService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/otp")
