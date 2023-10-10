a4ae52b384c0c040978653a016e7eb6812095ff8
1192913 <minalkrishna66@gmail.com>
2023-10-10 08:12:04 +0530

    goldloanservice

--- ApiGateway/src/main/java/com/natwest/ApiGateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/natwest/ApiGateway/ApiGatewayApplication.java
@@ -3,6 +3,9 @@ package com.natwest.ApiGateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +13,13 @@ public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+	@Bean
+	public RouteLocator apiRoutes(RouteLocatorBuilder builder){
+		return builder.routes()
+				.route("goldloanService_route",route->route.path("/api/v1/loan/gold/docs/**").uri("lb://GoldLoan_Service"))
+				.build();
+
 	}
 
 }
